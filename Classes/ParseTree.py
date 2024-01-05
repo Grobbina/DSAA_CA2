111b522ecb717a2f016d74033793666d2bf7b043
Cai Ruizhe <ruizhe.22@ichat.sp.edu.sg>
2024-01-05 15:49:01 +0800

    Parse Tree option 1 and stack

--- Classes/ParseTree.py
+++ Classes/ParseTree.py
@@ -1,10 +1,42 @@
 import regex as re
+from BinaryTree import BinaryTree
+from Stack import Stack
 
-class ParseTree():
+class ParseTree(BinaryTree):
     def __init__(self, expression):
         self.expression = expression
         self.tokens = re.findall(r'\d+|\+|\-|\*|\/|\*\*|\(|\)', expression)
         self.current_index = 0
 
-    def BuildTree(self):
-        return self.Expression()
+    def build(self):
+        stack = Stack()
+        tree = BinaryTree('?')
+        stack.push(tree)
+        currentTree = tree
+        for t in self.tokens:
+            if t == '(':
+                currentTree.insertLeft('?')
+                stack.push(currentTree)
+                currentTree = currentTree.getLeftTree()
+            elif t in ['+', '-', '*', '/']:
+                currentTree.setKey(t)
+                currentTree.insertRight('?')
+                stack.push(currentTree)
+                currentTree = currentTree.getRightTree()
+            elif t not in ['+', '-', '*', '/', ')'] :
+                currentTree.setKey(int(t))
+                parent = stack.pop()
+                currentTree = parent
+            elif t == ')':
+                currentTree = stack.pop()
+            else:
+                raise ValueError
+                return tree
+            
+
+
+if __name__ == '__main__':
+    tree = ParseTree('(3+(4*5))')
+    tree.build()
+
+    
