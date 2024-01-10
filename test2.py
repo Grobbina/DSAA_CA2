16111089d8855974727995730d31bd98c8a0210c
Cai Ruizhe <ruizhe904@gmail.com>
2024-01-10 13:34:29 +0800

    nah i'd win nah id win nah id win nah id win
    as the strongest curse jogoat fought the fraud the king of curses

--- test2.py
+++ test2.py
@@ -1,6 +1,13 @@
 import re 
+from Classes.ParseTree import ParseTree
 
-statement = '50-3x+50-60'
-#extract only numerics not followed by a letter but following a + or - sign
-matchno = re.findall(r'(?<=[+-])\d+(?![a-zA-Z])', statement)
+statement = '41'
+#extract only numerics not followed by a letter as well as the operator before
+matchno = re.findall(r'(\+?-?\d+(?![a-zA-Z]))', statement)
 print(matchno)
+evaluated = sum([int(i) for i in matchno])
+for match in matchno:
+    statement = statement.replace(match, '')
+statement = f'{evaluated}+{statement}'
+tree = ParseTree(statement)
+print(statement)
