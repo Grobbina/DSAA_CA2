2489d132b2196341b1b8b148dad4ffb08f0a98dd
Cai Ruizhe <ruizhe.22@ichat.sp.edu.sg>
2024-02-04 21:49:29 +0800

    valdiation

---
+++ Classes/validation.py
@@ -0,0 +1,37 @@
+import os 
+
+class validator():
+    def __init__(self):
+        pass
+
+    def filevalidation(self, path:str):
+        current_directory = os.getcwd()
+        file_list = os.listdir(current_directory)
+        if path == '':
+            return True
+        elif path not in file_list:
+            print('Invalid File Path')
+            return True
+        else:
+            return False
+    
+    def __checktxt(self, path:str):
+        if path.endswith('.txt'):
+            return True
+        else:
+            print('Invalid File Type')
+            return False
+        
+    def addtxt(self, path:str):
+        if self.__checktxt(path) == False:
+            addtxt = input(f'Did you mean {path}.txt? y/n: ')
+            if addtxt == 'y':
+                path += '.txt'
+                return path
+            else:
+                return path
+
+
+
+if __name__ == '__main__':
+    validator.filevalidation(path = 'Path to Check Here')
